02bbb9957e04c0bdd695e1348fbffd425d32740b
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-11-27 12:33:29 +0100

    chore: Update td_perms version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdDfLib.MixProject do
   def project do
     [
       app: :td_df_lib,
-      version: "2.8.0",
+      version: "2.8.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -24,7 +24,7 @@ defmodule TdDfLib.MixProject do
       {:phoenix_ecto, "~> 3.2"},
       {:poison, "~> 2.2.0"},
       {:credo, "~> 0.9.3", only: [:dev, :test], runtime: false},
-      {:td_perms, git: "https://github.com/Bluetab/td-perms.git", tag: "2.8.1"},
+      {:td_perms, git: "https://github.com/Bluetab/td-perms.git", tag: "2.8.2"},
     ]
   end
 end
