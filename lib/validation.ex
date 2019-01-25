d5803049867caec101a7e3082c33b7b0bca229a4
netto <euclydes.young@gmail.com>
2019-01-25 12:48:19 +0100

    feat: changed validations to new content format

--- lib/validation.ex
+++ lib/validation.ex
@@ -6,35 +6,35 @@ defmodule TdDfLib.Validation do
   alias Ecto.Changeset
   @df_cache Application.get_env(:td_df_lib, :df_cache)
 
-  @string "string"
-  @list "list"
-  @variable_list "variable_list"
-  @variable_map_list "variable_map_list"
-  @map_list "map_list"
+  @types %{
+    "string" => :string,
+    "boolean" => :boolean,
+    "url" => :map,
+    "user" => :string
+  }
 
   def get_content_changeset(content, template_name) do
-    schema = get_template_cotent(template_name)
+    schema = get_template_content(template_name)
     build_changeset(content, schema)
   end
 
-  defp get_template_cotent(template_name) do
+  defp get_template_content(template_name) do
     @df_cache.get_template_content(template_name)
   end
 
   def build_changeset(content, content_schema) do
     changeset_fields = get_changeset_fields(content_schema)
-    changeset = {content, changeset_fields}
-
-    changeset
-    |> Changeset.cast(content, Map.keys(changeset_fields))
-    |> add_content_validation(content_schema)
+    {content, changeset_fields}
+      |> Changeset.cast(content, Map.keys(changeset_fields))
+      |> add_content_validation(content_schema)
   end
 
   defp get_changeset_fields(content_schema) do
     item_mapping = fn item ->
       name = item |> Map.get("name")
-      type = item |> Map.get("type")
-      {String.to_atom(name), get_changeset_field(type)}
+      type = Map.get(@types, Map.get(item, "type"))
+      cardinality = item |> Map.get("cardinality")
+      {String.to_atom(name), get_field_type(type, cardinality)}
     end
 
     content_schema
@@ -42,21 +42,13 @@ defmodule TdDfLib.Validation do
     |> Map.new()
   end
 
-  defp get_changeset_field(type) do
-    case type do
-      @string -> :string
-      @list -> :string
-      @variable_list -> {:array, :string}
-      @variable_map_list -> {:array, :map}
-      @map_list -> :map
-    end
-  end
+  defp get_field_type(type, "*"), do: {:array, type}
+  defp get_field_type(type, "+"), do: {:array, type}
+  defp get_field_type(type, _), do: type
 
   # Filters schema for non applicable dependant field
-  defp add_content_validation(
-         changeset,
-         %{"depends" => %{"on" => depend_on, "to_be" => depend_to_be}} = content_item
-       ) do
+  defp add_content_validation(changeset, %{"depends" =>
+      %{"on" => depend_on, "to_be" => depend_to_be}} = content_item) do
     case Map.get(changeset.data, depend_on) do
       ^depend_to_be ->
         add_content_validation(changeset, Map.drop(content_item, ["depends"]))
@@ -69,8 +61,8 @@ defmodule TdDfLib.Validation do
   defp add_content_validation(changeset, %{} = content_item) do
     changeset
     |> add_require_validation(content_item)
-    |> add_max_length_validation(content_item)
-    |> add_inclusion_validation(content_item)
+    # |> add_max_length_validation(content_item)
+    # |> add_inclusion_validation(content_item)
   end
 
   defp add_content_validation(changeset, [tail | head]) do
@@ -81,8 +73,14 @@ defmodule TdDfLib.Validation do
 
   defp add_content_validation(changeset, []), do: changeset
 
-  defp add_require_validation(changeset, %{"name" => name, "required" => true}) do
-    Changeset.validate_required(changeset, [String.to_atom(name)])
+  defp add_require_validation(changeset, %{"name" => name, "cardinality" => "1"}) do
+    Changeset.validate_required(changeset, String.to_atom(name))
+  end
+
+  defp add_require_validation(changeset, %{"name" => name, "cardinality" => "+"}) do
+    changeset
+    |> Changeset.validate_required(String.to_atom(name))
+    |> Changeset.validate_length(String.to_atom(name), min: 1)
   end
 
   defp add_require_validation(changeset, %{}), do: changeset
