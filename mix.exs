59b6c5647f3df11d0ec59cf623a090caea7c4c00
netto <euclydes.young@bluetab.net>
2019-03-28 18:49:36 +0100

    chore: [TD-1606] bump td-perms version

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule TdDfLib.MixProject do
       {:phoenix_ecto, "~> 3.2"},
       {:poison, "~> 2.2.0"},
       {:credo, "~> 0.9.3", only: [:dev, :test], runtime: false},
-      {:td_perms, git: "https://github.com/Bluetab/td-perms.git", tag: "2.15.0"},
+      {:td_perms, git: "https://github.com/Bluetab/td-perms.git", tag: "2.16.0"},
     ]
   end
 end
