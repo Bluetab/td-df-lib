87c28cba269c49913159c37c8c812715d5150264
Tom Crossland <tom.crossland@bluetab.net>
2019-06-26 23:53:59 +0200

    chore: 3.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdDfLib.MixProject do
   def project do
     [
       app: :td_df_lib,
-      version: "2.21.4",
+      version: "3.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
