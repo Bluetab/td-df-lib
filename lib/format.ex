92843363adb9877ab517ce2da1a3658401b96923
saul.isern <saul.isern@bluetab.net>
2019-08-02 14:39:40 +0200

    chore: [TD-1560] credo issues

--- lib/format.ex
+++ lib/format.ex
@@ -7,7 +7,7 @@ defmodule TdDfLib.Format do
   def apply_template(nil, _), do: nil
 
   def apply_template(%{} = content, _) when map_size(content) == 0, do: nil
-  
+
   def apply_template(_, nil), do: nil
 
   def apply_template(%{} = content, fields) do
@@ -43,8 +43,8 @@ defmodule TdDfLib.Format do
     field_names = Enum.map(fields, &Map.get(&1, "name"))
 
     content
-      |> Map.take(field_names)
-      |> set_default_values(fields)
+    |> Map.take(field_names)
+    |> set_default_values(fields)
   end
 
   def set_default_values(content, fields) do
