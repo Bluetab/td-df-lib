6b1b97ab126379860972f310e25e694c65e679ea
netto <euclydes.young@gmail.com>
2020-02-18 11:51:09 +0100

    chore: update CHANGELOG.md and bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdDfLib.MixProject do
   def project do
     [
       app: :td_df_lib,
-      version: "3.14.0",
+      version: "3.16.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -30,7 +30,7 @@ defmodule TdDfLib.MixProject do
       {:phoenix_ecto, "~> 4.0"},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:td_cache,
-       git: "https://github.com/Bluetab/td-cache.git", tag: "3.8.0", only: [:test], runtime: false}
+       git: "https://github.com/Bluetab/td-cache.git", tag: "3.16.1", only: [:test], runtime: false}
     ]
   end
 end
