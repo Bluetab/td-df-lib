2b03bca699b78bc53cd20e751e4b001a949f5d0c
Saul Isern <saul.isern@bluetab.net>
2020-05-20 16:43:57 +0200

    chore: [TD-2629] omit fields of type image on search values

--- test/format_test.exs
+++ test/format_test.exs
@@ -116,15 +116,17 @@ defmodule TdDfLib.FormatTest do
       }
     }
 
-    fields = [%{
-      "name" => "group",
-      "fields" => [
-        %{"name" => "foo", "default" => "foo"},
-        %{"name" => "bar", "cardinality" => "+", "values" => []},
-        %{"name" => "baz", "cardinality" => "*", "values" => []},
-        %{"name" => "bay", "type" => "enriched_text"}
-      ]
-    }]
+    fields = [
+      %{
+        "name" => "group",
+        "fields" => [
+          %{"name" => "foo", "default" => "foo"},
+          %{"name" => "bar", "cardinality" => "+", "values" => []},
+          %{"name" => "baz", "cardinality" => "*", "values" => []},
+          %{"name" => "bay", "type" => "enriched_text"}
+        ]
+      }
+    ]
 
     assert Format.search_values(content, %{content: fields}) == %{
              "foo" => "foo",
@@ -141,12 +143,70 @@ defmodule TdDfLib.FormatTest do
 
   test "search_values/2 returns nil when no content is provided" do
     fields = [
-      %{"name" => "foo", "default" => "foo"},
-      %{"name" => "bar", "cardinality" => "+", "values" => []},
-      %{"name" => "baz", "cardinality" => "*", "values" => []}
+      %{
+        "name" => "group",
+        "fields" => [
+          %{"name" => "foo", "default" => "foo"},
+          %{"name" => "bar", "cardinality" => "+", "values" => []},
+          %{"name" => "baz", "cardinality" => "*", "values" => []},
+          %{"name" => "bay", "type" => "enriched_text"}
+        ]
+      }
     ]
 
     assert is_nil(Format.search_values(nil, fields))
+  end
+
+  test "search_values/2 omits values of type image" do
+    content = %{
+      "xyzzy" => "spqr",
+      "foo" => %{
+        "object" => "value",
+        "document" => %{
+          "data" => %{},
+          "nodes" => [
+            %{
+              "data" => %{},
+              "type" => "paragraph",
+              "nodes" => [
+                %{
+                  "text" => "My Text",
+                  "marks" => [
+                    %{
+                      "data" => %{},
+                      "type" => "bold",
+                      "object" => "mark"
+                    }
+                  ],
+                  "object" => "text"
+                }
+              ],
+              "object" => "block"
+            }
+          ],
+          "object" => "document"
+        }
+      },
+      "bay" => "photo code..."
+    }
+
+    fields = [
+      %{
+        "name" => "group",
+        "fields" => [
+          %{"name" => "foo", "type" => "enriched_text"},
+          %{"name" => "bar", "cardinality" => "+", "values" => []},
+          %{"name" => "baz", "cardinality" => "*", "values" => []},
+          %{"name" => "bay", "type" => "image"}
+        ]
+      }
+    ]
+
+    assert Format.search_values(content, %{content: fields}) == %{
+             "bar" => [""],
+             "baz" => [""],
+             "foo" => "My Text"
+           }
   end
 
   test "format_field returns url wrapped" do
@@ -176,9 +236,9 @@ defmodule TdDfLib.FormatTest do
   test "format_field of enriched_text returns wrapped enriched text" do
     formatted_value =
       Format.format_field(%{
-      "content" => "some enriched text",
-      "type" => "enriched_text"
-    })
+        "content" => "some enriched text",
+        "type" => "enriched_text"
+      })
 
     assert formatted_value == RichText.to_rich_text("some enriched text")
   end
@@ -189,24 +249,30 @@ defmodule TdDfLib.FormatTest do
         "name" => "group1",
         "fields" => [
           %{"name" => "field11", "label" => "label11", "type" => "string"},
-          %{"name" => "field12", "label" => "label12", "cardinality" => "+"},
+          %{"name" => "field12", "label" => "label12", "cardinality" => "+"}
         ]
       },
       %{
         "name" => "group2",
         "fields" => [
           %{"name" => "field21", "label" => "label21", "widget" => "default"},
-          %{"name" => "field22", "label" => "label22", "values" => %{"fixed" => ["a", "b", "c"]}},
+          %{"name" => "field22", "label" => "label22", "values" => %{"fixed" => ["a", "b", "c"]}}
         ]
-      },
+      }
     ]
 
     flat_content = Format.flatten_content_fields(content)
+
     expected_flat_content = [
       %{"group" => "group1", "name" => "field11", "label" => "label11", "type" => "string"},
       %{"group" => "group1", "name" => "field12", "label" => "label12", "cardinality" => "+"},
       %{"group" => "group2", "name" => "field21", "label" => "label21", "widget" => "default"},
-      %{"group" => "group2", "name" => "field22", "label" => "label22", "values" => %{"fixed" => ["a", "b", "c"]}},
+      %{
+        "group" => "group2",
+        "name" => "field22",
+        "label" => "label22",
+        "values" => %{"fixed" => ["a", "b", "c"]}
+      }
     ]
 
     assert flat_content == expected_flat_content
