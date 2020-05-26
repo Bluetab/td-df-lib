414f75be17391dce55b21a50f99dbf45a7fbb866
sc <sonia.curiel@bluetab.net>
2020-05-26 15:39:39 +0200

    chore: [TD-2629] version 3.23.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## [Unreleased]
+## [3.23.0] 2020-05-26
 
 ### Changed
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdDfLib.MixProject do
   def project do
     [
       app: :td_df_lib,
-      version: "3.16.0",
+      version: "3.23.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
