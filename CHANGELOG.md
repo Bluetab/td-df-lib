7a2bb9e03ec0f9538f48fbb6ebbbec26affbd9f9
netto <euclydes.young@gmail.com>
2022-01-04 18:13:17 +0000

    chore: bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## [4.35.0] 2022-01-04
 
 ### Added
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdDfLib.MixProject do
   def project do
     [
       app: :td_df_lib,
-      version: "4.31.0",
+      version: "4.35.0",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
