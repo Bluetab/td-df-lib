275e42ac83ae22ee3faadf6b9fb67394060f22d4
Tom Crossland <tom.crossland@bluetab.net>
2022-08-24 21:50:25 +0200

    chore: bump td_cache

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule TdDfLib.MixProject do
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ecto, "~> 3.0"},
       {:ex_machina, "~> 2.3", only: :test},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "4.48.1"}
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", branch: "feature/td-5091"}
     ]
   end
 end
