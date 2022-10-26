3fa4d56285762e9356c72d9cd7ecbdbdfccae0e7
Tom Crossland <tom.crossland@bluetab.net>
2022-10-26 13:01:40 +0200

    chore: bump deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [4.54.0] 2022-10-26
+
+### Changed
+
+- [TD-5284] Update dependencies
+
 ## [4.53.1] 2022-10-17
 
 ### Changed
--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule TdDfLib.MixProject do
   def project do
     [
       app: :td_df_lib,
-      version: "4.53.1",
-      elixir: "~> 1.11",
+      version: "4.54.0",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -32,7 +32,7 @@ defmodule TdDfLib.MixProject do
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ecto, "~> 3.0"},
       {:ex_machina, "~> 2.3", only: :test},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "4.50.0"}
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "4.54.0"},
     ]
   end
 end
