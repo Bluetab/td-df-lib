027f426c4eb3ec269996eab3518a2e1441cfead7
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2023-11-10 14:42:51 +0100

    chore: TD-6177 bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdDfLib.MixProject do
   def project do
     [
       app: :td_df_lib,
-      version: "5.17.0",
+      version: "5.18.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
