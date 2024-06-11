ed2962a36790d40b8d9c0be186848cb51127a09e
Juan Francisco Huete Verdejo <jf.huete@bluetab.net>
2024-06-11 15:47:16 +0200

    chore: TD-6440 Update changelog

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdDfLib.MixProject do
   def project do
     [
       app: :td_df_lib,
-      version: "6.5.1",
+      version: "6.7.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
