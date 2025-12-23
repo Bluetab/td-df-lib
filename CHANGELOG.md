1c6077c6ccd4cfd93a3e4f4d403f9a0c1aaa0824
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2025-12-23 15:49:50 +0100

    chore: TD-7760 bump version 8.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## [Unreleased]
+## [8.0.0] 2025-12-23
 
 ## Changed
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdDfLib.MixProject do
   def project do
     [
       app: :td_df_lib,
-      version: "7.14.0",
+      version: "8.0.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
